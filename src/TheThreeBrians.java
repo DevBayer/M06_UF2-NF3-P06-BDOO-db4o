b9eb474eefc4a41fef00c36bc8202d44a5e70f7b
DevBayer <cava.staff@gmail.com>
2017-02-13 15:48:01 +0100

    Initial commit

---
+++ src/TheThreeBrians.java
@@ -0,0 +1,64 @@
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Predicate;
+
+/**
+ * Created by Lluís Bayer Soler on 09/02/17.
+ */
+public class TheThreeBrians {
+    public static void main(String[] args)
+            throws Exception
+    {
+        ObjectContainer db = null;
+        try
+        {
+            db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "persons.data");
+
+            Person brian = new Person("Brian", "Goetz", 39);
+            Person jason = new Person("Jason", "Hunter", 35);
+            Person brians = new Person("Brian", "Sletten", 38);
+            Person david = new Person("David", "Geary", 55);
+            Person glenn = new Person("Glenn", "Vanderberg", 40);
+            Person neal = new Person("Neal", "Ford", 39);
+
+            db.store(brian);
+            db.store(jason);
+            db.store(brians);
+            db.store(david);
+            db.store(glenn);
+            db.store(neal);
+
+            db.commit();
+
+            // Find all the Brians
+            ObjectSet getBrians = db.query(new Predicate<Person>() {
+                @Override
+                public boolean match(Person person) {
+                    return person.getFirstName().startsWith("Brian");
+                }
+            });
+            while (getBrians.hasNext())
+                System.out.println(getBrians.next());
+
+            Person brian2 = new Person("Brian", "Goetz", 39);
+            db.store(brian2);
+            db.commit();
+
+            // Find all the Brians
+            getBrians = db.query(new Predicate<Person>() {
+                @Override
+                public boolean match(Person person) {
+                    return person.getFirstName().startsWith("Brian");
+                }
+            });
+            while (getBrians.hasNext())
+                System.out.println(getBrians.next());
+        }
+        finally
+        {
+            if (db != null)
+                db.close();
+        }
+    }
+}
